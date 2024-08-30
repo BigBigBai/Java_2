760da387acfad961a748e12fd27d22a90d9f02d5
BigBigBai <baiyingjian@outlook.com>
2024-08-30 00:23:29 -0400

    08/29

--- src/bigbigbai/_00_leetcode/_01_list/_2_AddTwoNumbers.java
+++ src/bigbigbai/_00_leetcode/_01_list/_2_AddTwoNumbers.java
@@ -1,6 +1,104 @@
 package bigbigbai._00_leetcode._01_list;
 
 public class _2_AddTwoNumbers {
+    public ListNode addNNumbers1(ListNode[] list) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode newTail = dummyNode;
+        int carry = 0;
+        int size = list.length;
+
+        while (true) {
+            int sum = carry;
+
+            for (int i = 0; i < size; i++) {
+                if (list[i] != null) {
+                    sum += list[i].val;
+                    list[i] = list[i].next;
+                }
+            }
+
+            carry = sum / 10;
+            newTail.next = new ListNode(sum % 10);
+            newTail = newTail.next;
+
+            if (carry == 0) break;
+        }
+        newTail.next = null;
+        return dummyNode.next;
+    }
+
+    public ListNode addNNumbers(ListNode[] list) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode newTail = dummyNode;
+        int carry = 0;
+        int size = list.length;
+
+        while (true) {
+            int sum = 0;
+
+            for (int i = 0; i < size; i++) {
+                if (list[i] != null) {
+                    sum += list[i].val;
+                    list[i] = list[i].next;
+                }
+            }
+
+            // 当所有ListNode为null时，break
+            if (sum == 0) break;
+
+            sum += carry;
+            carry = sum / 10;
+            newTail.next = new ListNode(sum % 10);
+            newTail = newTail.next;
+        }
+
+        while (carry > 0) {
+            int sum = carry;
+            carry = sum / 10;
+            newTail.next = new ListNode(sum % 10);
+            newTail = newTail.next;
+        }
+        newTail.next = null;
+        return dummyNode.next;
+    }
+
+    public ListNode addThreeNumbers(ListNode l1, ListNode l2, ListNode l3) {
+        ListNode dummyNode = new ListNode(0);
+        ListNode newTail = dummyNode;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || l3 != null) {
+            int sum = 0;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            if (l3 != null) {
+                sum += l3.val;
+                l3 = l3.next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+            newTail.next = new ListNode(sum % 10);
+            newTail = newTail.next;
+        }
+
+        if (carry > 0) {
+            newTail.next = new ListNode(carry);
+            newTail = newTail.next;
+        }
+        newTail.next = null;
+        return dummyNode.next;
+    }
+
     public ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
         ListNode dummyNode = new ListNode(0);
         ListNode newTail = dummyNode;
--- src/bigbigbai/_00_leetcode/_04_tree/bt/_107_BinaryTreeLevelOrderTraversal2.java
+++ src/bigbigbai/_00_leetcode/_04_tree/bt/_107_BinaryTreeLevelOrderTraversal2.java
@@ -2,34 +2,30 @@ package bigbigbai._00_leetcode._04_tree.bt;
 
 import bigbigbai._00_leetcode._04_tree.TreeNode;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
-public class _104_MaximumDepthOfBinaryTree {
-    // 非递归
-    public int maxDepth1(TreeNode root) {
-        if (root == null) return 0;
+public class _107_BinaryTreeLevelOrderTraversal2 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
         Queue<TreeNode> queue = new LinkedList<>();
+        if (root == null) return res;
         queue.add(root);
-        int depth = 0;
 
         while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
+                list.add(node.val);
                 if (node.left != null) queue.add(node.left);
                 if (node.right != null) queue.add(node.right);
             }
-            depth++;
+            res.add(0, list);
         }
 
-        return depth;
-    }
-
-    // 递归
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        return res;
     }
 }

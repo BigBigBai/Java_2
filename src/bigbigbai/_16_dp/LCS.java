14566c0c13966d232220bf825ad7053c601b2e6c
BigBigBai <baiyingjian@outlook.com>
2025-01-11 08:25:25 -0500

    16_dp_lcs

--- src/bigbigbai/_16_dp/LCS.java
+++ src/bigbigbai/_16_dp/LCS.java
@@ -8,9 +8,120 @@ public class LCS {
         int[] nums1 = {1,2,3,4,7};
         int[] nums2 = {1,3,5,7};
 
-        System.out.println(lcs(nums1, nums2));
+        System.out.println(lcs5(nums1, nums2));
     }
 
+    public static int lcs6(String text1, String text2) {
+        char[] nums1 = text1.toCharArray();
+        char[] nums2 = text2.toCharArray();
+
+        char[] rowsNums = nums1, colsNums = nums2;
+        if (nums1.length < nums2.length) {
+            colsNums = nums1;
+            rowsNums = nums2;
+        }
+
+        int[] dp = new int[colsNums.length + 1];
+        for (int i = 1; i <= rowsNums.length; i++) {
+            int cur = 0;
+            for (int j = 1; j <= colsNums.length; j++) {
+                int leftTop = cur;
+
+                cur = dp[j];
+                if (rowsNums[i-1] == colsNums[j-1]) dp[j] = leftTop + 1;
+                else dp[j] = Math.max(dp[j-1], dp[j]);
+            }
+        }
+
+        return dp[colsNums.length];
+    }
+
+    public static int lcs5(int[] nums1, int[] nums2) {
+        int[] rowsNums = nums1, colsNums = nums2;
+        if (nums1.length < nums2.length) {
+            colsNums = nums1;
+            rowsNums = nums2;
+        }
+
+        int[] dp = new int[colsNums.length + 1];
+        for (int i = 1; i <= rowsNums.length; i++) {
+            int cur = 0;
+            for (int j = 1; j <= colsNums.length; j++) {
+                int leftTop = cur;
+
+                cur = dp[j];
+                if (rowsNums[i-1] == colsNums[j-1]) dp[j] = leftTop + 1;
+                else dp[j] = Math.max(dp[j-1], dp[j]);
+            }
+        }
+
+        return dp[colsNums.length];
+    }
+
+
+    // 1D array
+    public static int lcs4(int[] nums1, int[] nums2) {
+        int[] dp = new int[nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            int cur = 0;
+            for (int j = 1; j <= nums2.length; j++) {
+                int leftTop = cur;
+
+                cur = dp[j];
+                if (nums1[i-1] == nums2[j-1]) dp[j] = leftTop + 1;
+                else dp[j] = Math.max(dp[j-1], dp[j]);
+            }
+        }
+
+        return dp[nums2.length];
+    }
+
+    // % 2 == & 1
+    public static int lcs3(int[] nums1, int[] nums2) {
+        int[][] dp = new int[2][nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                int row = i & 1;
+                int preRow = (i - 1) & 1;
+
+                if (nums1[i-1] == nums2[j-1]) dp[row][j] = dp[preRow][j-1] + 1;
+                else dp[row][j] = Math.max(dp[row][j-1], dp[preRow][j]);
+            }
+        }
+
+        return dp[nums1.length & 1][nums2.length];
+    }
+
+    // 2D array
+    public static int lcs2(int[] nums1, int[] nums2) {
+        int[][] dp = new int[2][nums2.length + 1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                int row = i % 2;
+                int preRow = (i - 1) % 2;
+
+                if (nums1[i-1] == nums2[j-1]) dp[row][j] = dp[preRow][j-1] + 1;
+                else dp[row][j] = Math.max(dp[row][j-1], dp[preRow][j]);
+            }
+        }
+
+        return dp[nums1.length % 2][nums2.length];
+    }
+
+    // m * n matrix
+    public static int lcs1(int[] nums1, int[] nums2) {
+        int[][]dp = new int[nums1.length+1][nums2.length+1];
+        for (int i = 1; i <= nums1.length; i++) {
+            for (int j = 1; j <= nums2.length; j++) {
+                if (nums1[i-1] == nums2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+            }
+        }
+
+        return dp[nums1.length][nums2.length];
+    }
+
+    // recursion
     public static int lcs(int[] nums1, int[] nums2) {
         return lcs(nums1, nums1.length, nums2, nums2.length);
     }

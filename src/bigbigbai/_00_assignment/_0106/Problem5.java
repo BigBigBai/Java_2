a174ed28be36fa81f69e042670272e2b1a7aae3b
BigBigBai <baiyingjian@outlook.com>
2025-01-16 21:28:33 -0500

    01/16

---
+++ src/bigbigbai/_00_assignment/_0106/Problem5.java
@@ -0,0 +1,24 @@
+package bigbigbai._00_assignment._0106;
+
+import java.util.Arrays;
+
+public class Problem5 {
+    public static void main(String[] args) {
+        String[][] matrix = {{"8", "9", "10"}, {"16", "X", "20"}, {"24", "X", "30"}};
+        System.out.println(Arrays.deepToString(solution(matrix)));
+    }
+
+    public static String[][] solution(String[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        String[][] res = new String[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+            }
+        }
+
+        return res;
+    }
+}

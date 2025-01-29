990072c3369ee1d7ecadfa55693f3727ce77de7d
BigBigBai <baiyingjian@outlook.com>
2025-01-28 21:19:16 -0500

    01/28

--- src/bigbigbai/_09_graph/ListGraph.java
+++ src/bigbigbai/_09_graph/ListGraph.java
@@ -1,10 +1,11 @@
 package bigbigbai._09_graph;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class ListGraph<V, E> extends Graph<V, E> {
+    private Set<Edge<V, E>> edges = new HashSet<>();
+    private Map<V, Vertex<V, E>> vertices = new HashMap<>();
+
     private static class Vertex<V, E> {
         V value;
         Set<Edge<V, E>> inEdges = new HashSet<>();
@@ -24,7 +25,7 @@ public class ListGraph<V, E> extends Graph<V, E> {
 
         @Override
         public int hashCode() {
-            return Objects.hash(value, inEdges, outEdges);
+            return value != null ? value.hashCode() : 0;
         }
     }
 
@@ -49,42 +50,108 @@ public class ListGraph<V, E> extends Graph<V, E> {
 
         @Override
         public int hashCode() {
-            return Objects.hash(from, to, weight);
+            return from.hashCode() * 31 + to.hashCode() * 31 + weight.hashCode();
         }
     }
 
     @Override
     public int verticesSize() {
-        return 0;
+        return vertices.size();
     }
 
     @Override
     public int edgesSize() {
-        return 0;
+        return edges.size();
     }
 
     @Override
-    public void addVertex(Object o) {
-
+    public void addVertex(V v) {
+        if (vertices.containsKey(v)) return;
+        vertices.put(v, new Vertex<>(v));
     }
 
     @Override
-    public void removeVertex(Object o) {
+    public void removeVertex(V v) {
+        Vertex<V, E> vertex = vertices.remove(v);
+        if (vertex == null) return;
 
+        // for collection set deletion, cannot loop while deleting
+        // has ConcurrentModificationException
+        // Soln: use Iterator to iterate and remove!
+//        for (Edge<V, E> edge : vertex.outEdges) {
+//            removeEdge(edge.from.value, edge.to.value);
+//        }
+//
+//        for (Edge<V, E> edge : vertex.inEdges) {
+//            removeEdge(edge.from.value, edge.to.value);
+//        }
+
+        for (Iterator<Edge<V, E>> iterator = vertex.outEdges.iterator(); iterator.hasNext();) {
+            Edge<V, E> edge = iterator.next();
+            // 666!
+            edge.to.inEdges.remove(edge);
+            iterator.remove();
+            edges.remove(edge);
+        }
+
+        for (Iterator<Edge<V, E>> iterator = vertex.inEdges.iterator(); iterator.hasNext();) {
+            Edge<V, E> edge = iterator.next();
+            // 666!
+            edge.from.outEdges.remove(edge);
+            iterator.remove();
+            edges.remove(edge);
+        }
     }
 
     @Override
-    public void addEdge(Object fromV, Object toV) {
-
+    public void addEdge(V fromV, V toV) {
+        addEdge(fromV, toV, null);
     }
 
     @Override
-    public void addEdge(Object fromV, Object toV, Object weight) {
+    public void addEdge(V fromV, V toV, E weight) {
+        // 1. test if fromV, toV exist
+        Vertex<V, E> fromVertex = vertices.get(fromV);
+        if (fromVertex == null) {
+            fromVertex = new Vertex<>(fromV);
+            vertices.put(fromV, fromVertex);
+        }
 
+        Vertex<V, E> toVertex = vertices.get(toV);
+        if (toVertex == null) {
+            toVertex = new Vertex<>(toV);
+            vertices.put(toV, toVertex);
+        }
+
+        // 2. code can come here, demonstrate that this edge's start vertex and end vertex must exist
+        Edge<V, E> edge = new Edge<>(fromVertex, toVertex);
+        edge.weight = weight;
+
+        if (fromVertex.outEdges.remove(edge)) {
+            toVertex.inEdges.remove(edge);
+            edges.remove(edge);
+        }
+
+        fromVertex.outEdges.add(edge);
+        toVertex.inEdges.add(edge);
+        edges.add(edge);
     }
 
     @Override
-    public void removeEdge(Object fromV, Object toV) {
+    public void removeEdge(V fromV, V toV) {
+        // 1. test if fromV, toV exist
+        Vertex<V, E> fromVertex = vertices.get(fromV);
+        if (fromVertex == null) return;
 
+        Vertex<V, E> toVertex = vertices.get(toV);
+        if (toVertex == null) return;
+
+        // 2. code can come here, demonstrate that this edge's start vertex and end vertex must exist
+        Edge<V, E> edge = new Edge<>(fromVertex, toVertex);
+
+        if (fromVertex.outEdges.remove(edge)) {
+            toVertex.inEdges.remove(edge);
+            edges.remove(edge);
+        }
     }
 }
